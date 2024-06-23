475332f9e707484d594017421ef6a48469fb0f99
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2024-06-23 23:12:56 +0000

    Make of products controller and routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import produitRouter from './routers/produitRoutes.js'
 
 dotenv.config()
 
@@ -8,6 +9,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(produitRouter)
 
 app.get("/", (req, res) => {
   res.json({
